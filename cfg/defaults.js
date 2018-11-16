945570e84953a7d69d5fea28f30e04be8e8adae0
bcxiaolang008 <360569102@qq.com>
2018-11-16 17:42:40 +0800

    项目添加数据和scss样式

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -26,7 +26,7 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
       },
       {
         test: /\.sass/,
@@ -34,7 +34,7 @@ function getDefaultModules() {
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
@@ -47,6 +47,10 @@ function getDefaultModules() {
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
         loader: 'url-loader?limit=8192'
+      },
+      {
+         test: /\.json$/,
+         loader: 'json-loader'
       },
       {
         test: /\.(mp4|ogg|svg)$/,
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+// require('styles/App.css');
+require('styles/main.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+// 获取图片相关数据
+var imageDatas = require('../data/imageDatas.json');
+
+// 利用自执行函数，将图片信息转换成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+  for (let i = 0, j = imageDatasArr.length; i < j; i++) {
+    var singleImagaeData = imageDatasArr[i];
+    singleImagaeData.imageURL = require('../images/' + singleImagaeData.fileName);
+    imageDatasArr[i] = singleImagaeData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      // <div className="index">
+      //   <img src={yeomanImage} alt="Yeoman Generator" />
+      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      // </div>
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
     );
   }
 }
